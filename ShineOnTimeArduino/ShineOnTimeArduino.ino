72fa69e47d4bb851340f585e9eeabae86feee50a
SUI298 <sam.pitt@ik.me>
2023-04-09 13:45:49 +0200

    added ability to disable alarm remotely

--- ShineOnTimeArduino/ShineOnTimeArduino.ino
+++ ShineOnTimeArduino/ShineOnTimeArduino.ino
@@ -4,9 +4,10 @@
 //IO
 #define LED_PIN 10
 #define POTENTIOMETER_PIN A0
+#define POTENTIOMETER_SAMPLE_SIZE 8
 
 //OTHER
-#define MIN_BRIGHTNESS 16
+#define MIN_BRIGHTNESS 1
 #define BRIGHTNESS_INCREMENT_DELAY_x10ms 586 //Time to turn the light fully on = t_delay x n_increments = 586x10ms x 2^8 = 25 minutes
 #define BLINK_PERIOD_x10ms 20 //200ms
 #define NUM_BLINKS 4 //even number
@@ -17,37 +18,15 @@
 #define HOUR_UUID "67113022-c0e4-11ed-afa1-0242ac120002"
 #define MINUTE_UUID "67113d88-c0e4-11ed-afa1-0242ac120002"
 #define SECOND_UUID "671145d0-c0e4-11ed-afa1-0242ac120002"
+#define ALARM_ON_UUID "67114fa0-c0e4-11ed-afa1-0242ac120002"
 
-class RunningAverage{
-  #define N 100
-  uint8_t values[N];
-  uint8_t index;
-  public:
-  RunningAverage(uint8_t initial_val = 0){
-    index = 0;
-    for(int i = 0; i < N; i++){
-      values[i] = initial_val;
-    }
-  }
-  void addVal(uint8_t val){
-    values[index++] = val;
-    index %= N;    
-  }
-  uint8_t value(){
-    uint16_t sum = 0;
-    for(int i = 0; i < N; i++){
-      sum += values[i];
-    }
-    return sum/N;
-  }
-
-};
 
 BLEService alarmService(ALARM_TIME_UUID);
 BLEService timeService(CURRENT_TIME_UUID);
 
 BLEUnsignedCharCharacteristic alarmHourCharacteristic(HOUR_UUID, BLERead | BLEWrite);
 BLEUnsignedCharCharacteristic alarmMinuteCharacteristic(MINUTE_UUID, BLERead | BLEWrite);
+BLEBoolCharacteristic         alarmOnCharacteristic(ALARM_ON_UUID, BLERead | BLEWrite);
 BLEUnsignedCharCharacteristic timeHourCharacteristic(HOUR_UUID, BLERead | BLEWrite);
 BLEUnsignedCharCharacteristic timeMinuteCharacteristic(MINUTE_UUID, BLERead | BLEWrite);
 BLEUnsignedCharCharacteristic timeSecondCharacteristic(SECOND_UUID, BLERead | BLEWrite);
@@ -56,7 +35,7 @@ BLEDevice central;
 
 RTCZero clock;
 
-RunningAverage pot_avg;
+uint16_t pot_avg;
 uint8_t brightness;
 
 //flags
@@ -71,13 +50,12 @@ void brightnessControllSM(){
   static State state = CTRL_BY_POTENTIOMETER;
   static uint8_t num_blinks = 0;
   static uint8_t blink_brightness = 0xFF;
-  uint8_t potentiometer_val = pot_avg.value();
+  uint8_t user_brightness = pot_avg*pot_avg/0xFF; //non linear scale
   switch(state){
     default:
     case State::CTRL_BY_POTENTIOMETER:
-      brightness = constrain(map(potentiometer_val, MIN_BRIGHTNESS, 0xFF, 0, 0xFF),0 ,0xFF); //DO
+      brightness = user_brightness; //DO
       if(alarm){ //EVENT           
-        Serial.println("ALARM ALARM ALARM");
         brightness = 0; //ACTION
         state = State::CTRL_BY_ALARM;
       }
@@ -91,7 +69,7 @@ void brightnessControllSM(){
           &&  brightness < 0xFF ){
         brightness++;
       }
-      if( potentiometer_val > MIN_BRIGHTNESS){ //EVENT
+      if( (user_brightness > MIN_BRIGHTNESS) || !alarm){ //EVENT
         state = State::CTRL_BY_POTENTIOMETER;
         //
         alarm = false;
@@ -116,7 +94,9 @@ void brightnessControllSM(){
 void alarmMatchISR(){
   alarm = true;
   clock.disableAlarm();
+  alarmOnCharacteristic.writeValue(false);
 }
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_PIN,OUTPUT);
@@ -139,6 +119,7 @@ void setup() {
   // add the characteristic to the service
   alarmService.addCharacteristic(alarmHourCharacteristic);
   alarmService.addCharacteristic(alarmMinuteCharacteristic);
+  alarmService.addCharacteristic(alarmOnCharacteristic);
 
   timeService.addCharacteristic(timeHourCharacteristic);
   timeService.addCharacteristic(timeMinuteCharacteristic);
@@ -156,17 +137,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   brightnessControllSM();
-  pot_avg.addVal(analogRead(POTENTIOMETER_PIN)); 
   analogWrite(LED_PIN, brightness);
-  Serial.println(millis());
+
+  pot_avg = 0;
+  for(int i = 0; i < POTENTIOMETER_SAMPLE_SIZE; i++){
+    pot_avg += analogRead(POTENTIOMETER_PIN);
+  }
+  pot_avg /= POTENTIOMETER_SAMPLE_SIZE;
 
   if(!connected){
     central = BLE.central();
   }
   if(central){
-    if(!connected){
+    if(!connected){ //connection is new
       connected = true;
       Serial.print("Connected to central: ");
       // print the central's MAC address:
@@ -175,7 +160,7 @@ void loop() {
       blink = 6;
     }
     if(central.connected()){
-       if(timeHourCharacteristic.written()){
+      if(timeHourCharacteristic.written()){
         clock.setHours(timeHourCharacteristic.value());
       }
       if(timeMinuteCharacteristic.written()){
@@ -188,17 +173,25 @@ void loop() {
         clock.setAlarmHours(alarmHourCharacteristic.value());
       }
       if(alarmMinuteCharacteristic.written()){
-        Serial.println("alarm set.");
         clock.setAlarmMinutes(alarmMinuteCharacteristic.value());
-        clock.setAlarmSeconds(0);
-        clock.enableAlarm(clock.MATCH_HHMMSS);
-        blink = 2;
+      }
+      if(alarmOnCharacteristic.written()){
+        //Alarm enabled
+        if(alarmOnCharacteristic.value()){
+          Serial.println("alarm set.");
+          clock.setAlarmSeconds(0);
+          clock.enableAlarm(clock.MATCH_HHMMSS);
+          blink = 2;
+        }else{
+          //Alarm disabled
+          clock.disableAlarm();
+          alarm = false;
+          blink = 4;
+        }
       }
     }else{
       connected = false;
     }
   }
-
-  delay(1);
   while(millis()%LOOP_PERIOD_MS); //wait till next loop
 }
